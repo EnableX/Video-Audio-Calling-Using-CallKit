1d7d42ba6c5e46be7250775aa0e6145697065f6b
jaykumarvcx <jayprakash.k@vcloudx.com>
2025-07-30 10:36:03 +0530

    Updated with latest video sdks and also minimum target set for 15

---
+++ sampleCallkit/Pods/Target Support Files/Pods-sampleCallkitTests/Pods-sampleCallkitTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_sampleCallkitTests : NSObject
+@end
+@implementation PodsDummy_Pods_sampleCallkitTests
+@end
---
+++ sampleCallkit/Pods/Target Support Files/Pods-sampleCallkitUITests/Pods-sampleCallkitUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_sampleCallkitUITests : NSObject
+@end
+@implementation PodsDummy_Pods_sampleCallkitUITests
+@end
